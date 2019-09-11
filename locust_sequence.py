b9c42522c9d4effcc30250c6dcf93e4b86d427e6
Damien Diederen <dd@crosstwine.com>
2019-09-11 12:49:47 +0200

    locust_sequence.py: A single locust which runs all task sets in sequence

---
+++ locust_sequence.py
@@ -0,0 +1,59 @@
+from datetime import timedelta
+
+from zk_locust import ZKLocust, ZKLocustTaskSequence
+from locust_extra.stats import register_extra_stats
+from zk_metrics import register_zk_metrics
+
+from zk_locust.ops import duration
+from zk_locust.task_sets import ZKConnectTaskSet, ZKSetTaskSet, ZKGetTaskSet, ZKSetAndGetTaskSet, ZKCreateAndDeleteTaskSet, ZKWatchTaskSet, ZKExistsTaskSet, ZKExistsManyTaskSet, ZKGetChildrenTaskSet, ZKGetChildren2TaskSet
+
+register_extra_stats()
+register_zk_metrics()
+
+
+class Sequence(ZKLocust):
+    class task_set(ZKLocustTaskSequence):
+        def __init__(self, parent):
+            super(Sequence.task_set, self).__init__(parent)
+
+            s = timedelta(seconds=5)
+
+            def run_connect(task_set):
+                ZKConnectTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_set(task_set):
+                ZKSetTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_get(task_set):
+                ZKGetTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_set_and_get(task_set):
+                ZKSetAndGetTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_create_and_delete(task_set):
+                ZKCreateAndDeleteTaskSet(
+                    task_set, maybe_interrupt=duration(s)).run()
+
+            def run_watch(task_set):
+                ZKWatchTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_exists(task_set):
+                ZKExistsTaskSet(task_set, maybe_interrupt=duration(s)).run()
+
+            def run_exists_many(task_set):
+                ZKExistsManyTaskSet(
+                    task_set, maybe_interrupt=duration(s)).run()
+
+            def run_get_children(task_set):
+                ZKGetChildrenTaskSet(
+                    task_set, maybe_interrupt=duration(s)).run()
+
+            def run_get_children2(task_set):
+                ZKGetChildren2TaskSet(
+                    task_set, maybe_interrupt=duration(s)).run()
+
+            self.tasks = [
+                run_set, run_get, run_set_and_get, run_create_and_delete,
+                run_watch, run_exists, run_exists_many, run_get_children,
+                run_get_children2
+            ]
