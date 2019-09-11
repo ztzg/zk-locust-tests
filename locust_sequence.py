4356d8e80d291b3762f5fe5397efd503fab26e66
Damien Diederen <dd@crosstwine.com>
2019-09-11 17:42:34 +0200

    locust_sequence.py: Use double barrier around tasks

--- locust_sequence.py
+++ locust_sequence.py
@@ -3,7 +3,10 @@ from datetime import timedelta
 import gevent
 from gevent import GreenletExit
 
+from kazoo.recipe.barrier import DoubleBarrier
+
 import locust.runners
+from locust import events
 
 from zk_locust import ZKLocust, ZKLocustTaskSequence
 from locust_extra.stats import register_extra_stats
@@ -15,9 +18,12 @@ from zk_locust.task_sets import ZKConnectTaskSet, ZKSetTaskSet, ZKGetTaskSet, ZK
 register_extra_stats()
 register_zk_metrics()
 
+_num_clients = None
+
 
 def startup(*args, **kwargs):
-    gevent.sleep(10)
+    while _num_clients is None:
+        gevent.sleep(0.5)
 
 
 def shutdown(*args, **kwargs):
@@ -26,6 +32,35 @@ def shutdown(*args, **kwargs):
         locust_runner.quit()
     else:
         raise GreenletExit()
+
+
+def on_hatch_complete(user_count):
+    global _num_clients
+    _num_clients = user_count
+
+
+events.hatch_complete += on_hatch_complete
+
+_barrier_path = '/locust_sequence'
+
+
+def barrier_wrap(task):
+    def fn(task_set):
+        if _num_clients is None or _num_clients <= 0:
+            raise Exception('Invalid client count: %s' % _num_clients)
+        # Should we use another client?  (Possibly targeting another
+        # ensemble!)
+        zk_client = task_set.client.get_zk_client()
+        barrier = DoubleBarrier(zk_client, _barrier_path, _num_clients)
+        barrier.enter()
+        try:
+            gevent.sleep(0.5)
+            task(task_set)
+            gevent.sleep(0.5)
+        finally:
+            barrier.leave()
+
+    return fn
 
 
 class Sequence(ZKLocust):
@@ -69,8 +104,13 @@ class Sequence(ZKLocust):
                 ZKGetChildren2TaskSet(
                     task_set, maybe_interrupt=duration(s)).run()
 
-            self.tasks = [
-                startup, run_set, run_get, run_set_and_get,
-                run_create_and_delete, run_watch, run_exists, run_exists_many,
-                run_get_children, run_get_children2, shutdown
+            tasks = [
+                run_set, run_get, run_set_and_get, run_create_and_delete,
+                run_watch, run_exists, run_exists_many, run_get_children,
+                run_get_children2
             ]
+
+            if _barrier_path:
+                tasks = [barrier_wrap(task) for task in tasks]
+
+            self.tasks = [startup] + tasks + [shutdown]
