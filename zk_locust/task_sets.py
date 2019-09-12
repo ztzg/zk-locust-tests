3fda0076c0eb2df98e032f0b1ee8d356dd75884f
Damien Diederen <dd@crosstwine.com>
2019-09-12 10:04:09 +0200

    zk_locust/task_sets.py: Pass down task_set_name when creating ops

--- zk_locust/task_sets.py
+++ zk_locust/task_sets.py
@@ -5,38 +5,38 @@ from zk_locust.ops import ZKSetOp, ZKIncrementingSetOp, ZKGetOp, ZKConnectOp, ZK
 
 
 class ZKConnectTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKConnectTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='connect', **kwargs):
+        super(ZKConnectTaskSet, self).__init__(parent, **kwargs)
 
-        connect_op = ZKConnectOp(self.client)
+        connect_op = ZKConnectOp(self.client, task_set_name=name)
 
         self.tasks = [connect_op.task]
 
 
 class ZKSetTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKSetTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='set', **kwargs):
+        super(ZKSetTaskSet, self).__init__(parent, **kwargs)
 
-        op = ZKSetOp(self.client)
+        op = ZKSetOp(self.client, task_set_name=name)
 
         self.tasks = [op.task]
 
 
 class ZKGetTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKGetTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='get', **kwargs):
+        super(ZKGetTaskSet, self).__init__(parent, **kwargs)
 
-        op = ZKGetOp(self.client)
+        op = ZKGetOp(self.client, task_set_name=name)
 
         self.tasks = [op.task]
 
 
 class ZKSetAndGetTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKSetAndGetTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='set_and_get', **kwargs):
+        super(ZKSetAndGetTaskSet, self).__init__(parent, **kwargs)
 
-        set_op = ZKIncrementingSetOp(self.client)
-        get_op = ZKGetOp(self.client, request_type='get_incr_set')
+        set_op = ZKIncrementingSetOp(self.client, task_set_name=name)
+        get_op = ZKGetOp(self.client, task_set_name=name)
 
         # KLUDGE: Locust's dictionary approach does not work with
         # constructors.
@@ -44,14 +44,16 @@ class ZKSetAndGetTaskSet(ZKLocustTaskSet):
 
 
 class ZKCreateAndDeleteTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKCreateAndDeleteTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='create_and_delete', **kwargs):
+        super(ZKCreateAndDeleteTaskSet, self).__init__(parent, **kwargs)
 
         do_delete = deque()
 
-        create_op = ZKCreateEphemeralOp(self.client, push=do_delete.append)
-        delete_op = ZKDeleteFromQueueOp(self.client, pop=do_delete.popleft)
-        count_op = ZKCountChildrenOp(self.client)
+        create_op = ZKCreateEphemeralOp(
+            self.client, push=do_delete.append, task_set_name=name)
+        delete_op = ZKDeleteFromQueueOp(
+            self.client, pop=do_delete.popleft, task_set_name=name)
+        count_op = ZKCountChildrenOp(self.client, task_set_name=name)
 
         # KLUDGE: Locust's dictionary approach does not work with
         # constructors.
@@ -62,64 +64,76 @@ class ZKCreateAndDeleteTaskSet(ZKLocustTaskSet):
 
 
 class ZKWatchTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKWatchTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='watch', **kwargs):
+        super(ZKWatchTaskSet, self).__init__(parent, **kwargs)
 
         path = self.client.create_default_node()
 
-        op = ZKWatchOp(self.client, path)
+        op = ZKWatchOp(self.client, path, task_set_name=name)
 
         self.tasks = [op.task]
 
 
 class ZKExistsTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKExistsTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='exists', **kwargs):
+        super(ZKExistsTaskSet, self).__init__(parent, **kwargs)
 
         good_path = self.client.create_default_node()
         bad_path = self.client.join_path('/doesnotexist')
 
         pos_op = ZKExistsOp(
-            self.client, good_path, request_type='exists_positive')
+            self.client,
+            good_path,
+            request_type='exists_positive',
+            task_set_name=name)
         neg_op = ZKExistsOp(
-            self.client, bad_path, request_type='exists_negative')
+            self.client,
+            bad_path,
+            request_type='exists_negative',
+            task_set_name=name)
 
         posw_op = ZKExistsWithWatchOp(
-            self.client, good_path, request_type='exists_positive_watch')
+            self.client,
+            good_path,
+            request_type='exists_positive_watch',
+            task_set_name=name)
         negw_op = ZKExistsWithWatchOp(
-            self.client, bad_path, request_type='exists_negative_watch')
+            self.client,
+            bad_path,
+            request_type='exists_negative_watch',
+            task_set_name=name)
 
         self.tasks = [pos_op.task, neg_op.task, posw_op.task, negw_op.task]
 
 
 class ZKExistsManyTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKExistsManyTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='exists_many', **kwargs):
+        super(ZKExistsManyTaskSet, self).__init__(parent, **kwargs)
 
-        op = ZKExistsWithManyWatchesOp(self.client)
+        op = ZKExistsWithManyWatchesOp(self.client, task_set_name=name)
 
         self.tasks = [op.task]
 
 
 class ZKGetChildrenTaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKGetChildrenTaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='get_children', **kwargs):
+        super(ZKGetChildrenTaskSet, self).__init__(parent, **kwargs)
 
         path = self.client.join_path('/')
         self.client.create_default_node()
 
-        op = ZKGetChildrenOp(self.client, path)
+        op = ZKGetChildrenOp(self.client, path, task_set_name=name)
 
         self.tasks = [op.task]
 
 
 class ZKGetChildren2TaskSet(ZKLocustTaskSet):
-    def __init__(self, *args, **kwargs):
-        super(ZKGetChildren2TaskSet, self).__init__(*args, **kwargs)
+    def __init__(self, parent, *, name='get_children2', **kwargs):
+        super(ZKGetChildren2TaskSet, self).__init__(parent, **kwargs)
 
         path = self.client.join_path('/')
         self.client.create_default_node()
 
-        op = ZKGetChildren2Op(self.client, path)
+        op = ZKGetChildren2Op(self.client, path, task_set_name=name)
 
         self.tasks = [op.task]
