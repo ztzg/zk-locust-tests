e80b167f03b3bad8ee9972cb0f129f49da18630f
Damien Diederen <dd@crosstwine.com>
2019-09-24 19:35:23 +0200

    report: Understand that 'UNNAMED_OP' refers to N/A entries

--- report/extract_ls_subset_csv.py
+++ report/extract_ls_subset_csv.py
@@ -8,7 +8,12 @@ def main(executable, csv_path, task_set_and_op, subset_csv_path):
     task_set, op = task_set_and_op.split('/')[-2:]
     df = pd.read_csv(csv_path, index_col='timestamp', parse_dates=True)
 
-    df1 = df[(df.name == task_set) & (df.method == op)]
+    task_set_idx = df.name == task_set
+    # "UNNAMED_OP" currently selects N/A-containing records.  See note
+    # in gen_subsets_mk.py
+    op_idx = df.method == op if op != 'UNNAMED_OP' else df.method.isna()
+
+    df1 = df[task_set_idx & op_idx]
     req_diff = df1['num_requests'].diff()
     df2 = df1[req_diff.isnull() | req_diff > 0]
     df2.to_csv(subset_csv_path)
