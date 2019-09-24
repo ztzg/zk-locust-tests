ee72aac805103b56585ee857e382487638cf30b4
Damien Diederen <dd@crosstwine.com>
2019-09-24 19:50:09 +0200

    report: Fix: Do not choke on empty dataframes when extracting ZooKeeper metrics

--- report/extract_zkm_subset_csv.py
+++ report/extract_zkm_subset_csv.py
@@ -9,12 +9,16 @@ def main(executable, ls_subset_csv_path, zkm_csv_path, task_set_and_op,
     task_set, op = task_set_and_op.split('/')[-2:]
     ls_df = pd.read_csv(
         ls_subset_csv_path, index_col='timestamp', parse_dates=True)
-    min_ls = min(ls_df.index)
-    max_ls = max(ls_df.index)
 
     zkm_df = pd.read_csv(zkm_csv_path, index_col='timestamp', parse_dates=True)
 
-    subset_df = zkm_df[(zkm_df.index >= min_ls) & (zkm_df.index <= max_ls)]
+    if len(ls_df) > 0:
+        min_ls = min(ls_df.index)
+        max_ls = max(ls_df.index)
+
+        subset_df = zkm_df[(zkm_df.index >= min_ls) & (zkm_df.index <= max_ls)]
+    else:
+        subset_df = zkm_df.head(0)  # Only metadata
 
     subset_df.to_csv(zkm_subset_csv_path)
 
