102e87cc146ff73f961a80dab243581f4da059c8
Damien Diederen <dd@crosstwine.com>
2019-10-15 20:37:41 +0200

    locust_extra/control.py: Introduce new "programmed" handler
    Analogous to the one from zk_dispatch.

--- locust_extra/control.py
+++ locust_extra/control.py
@@ -1,13 +1,20 @@
+import os
+import re
+import logging
+
 import gevent
 
 import locust.runners
 from locust import events
+
+_logger = logging.getLogger(__name__)
 
 controller_available = events.EventHook()
 
 RUNNER_LOCAL, RUNNER_MASTER, RUNNER_SLAVE = ["local", "master", "slave"]
 
 _initial_hatch_complete = False
+_config_program = os.getenv('LOCUST_EXTRA_CONTROL_PROGRAM')
 
 
 def on_hatch_complete(user_count):
@@ -38,9 +45,9 @@ class Controller(object):
         self.runner = runner
         self.client_count = None
 
-    def wait_initial_hatch_complete(self):
+    def wait_initial_hatch_complete(self, sleep_ms=250):
         while not _initial_hatch_complete:
-            gevent.sleep(0.25)
+            gevent.sleep(sleep_ms / 1000)
 
     def get_num_clients(self):
         return self.runner.num_clients
@@ -58,11 +65,125 @@ class Controller(object):
         runner.start_hatching(num_clients, hatch_rate)
 
 
+class ProgrammedHandler(object):
+    def __init__(self, *, program):
+        if isinstance(program, str):
+            self.program = self._parse(program)
+        else:
+            self.program = program
+
+        self.pc = 0
+        self.controller = None
+
+    def run(self, controller):
+        self.controller = controller
+        self.min_num_clients = controller.get_num_clients()
+        self.max_num_clients = self.min_num_clients * 4
+        self.factor = None
+        while True:
+            instr = self.program[self.pc]
+            _logger.debug("Executing instruction[%d]: %s" % (self.pc, instr))
+            f = getattr(self, '_op_' + instr[0])
+            f(*instr[1:])
+            self.pc += 1
+            if self.pc >= len(self.program):
+                self.pc = 0
+
+    def sleep_ms(self, ms, cause=None):
+        msg = 'Sleeping %dms' % ms
+        if cause:
+            msg += ' ' + cause
+        _logger.debug(msg)
+        gevent.sleep(ms / 1000)
+
+    def change_num_clients(self, num_clients, hatch_rate=None):
+        num_clients = min(self.max_num_clients,
+                          max(self.min_num_clients, num_clients))
+        old_num_clients = self.controller.get_num_clients()
+        delta = num_clients - old_num_clients
+
+        if not delta:
+            _logger.debug('Client count unchanged: %d; constraint: [%d,%d]',
+                          old_num_clients, self.min_num_clients,
+                          self.max_num_clients)
+            return
+
+        hatch_rate = max(delta, 0)
+
+        _logger.debug('Changing client count: %d -> %d (%+d; hatch_rate=%d)',
+                      old_num_clients, num_clients, delta, hatch_rate)
+        self.controller.start_hatching(
+            num_clients=num_clients, hatch_rate=hatch_rate)
+
+    def _op_poll_initial_hatch_complete(self, sleep_ms):
+        while not _initial_hatch_complete:
+            self.sleep_ms(int(sleep_ms))
+
+    def _op_sleep(self, sleep_ms):
+        self.sleep_ms(int(sleep_ms))
+
+    def _op_set_min_num_clients(self, s):
+        self.min_num_clients = int(s)
+
+    def _op_set_max_num_clients(self, s):
+        self.max_num_clients = int(s)
+
+    def _op_change_num_clients(self, num_clients_str, hatch_rate_str=None):
+        hatch_rate = int(hatch_rate_str) if hatch_rate_str else None
+        self.change_num_clients(int(num_clients_str), hatch_rate)
+
+    def _op_multiply_num_clients(self, f=None):
+        f = self.factor if f is None else float(f)
+        if f is None:
+            _logger.error('Cannot multiply_num_clients without factor')
+            return
+        num_clients = self.controller.get_num_clients()
+        f_num_clients = num_clients * f
+        _logger.debug('num_clients: %d, f_num_clients: %d, f: %g', num_clients,
+                      f_num_clients, f)
+        self.change_num_clients(int(f_num_clients))
+
+    def _op_flip_at_bound(self, base_f=None):
+        num_clients = self.controller.get_num_clients()
+        if self.factor is not None and (num_clients > self.min_num_clients and
+                                        num_clients < self.max_num_clients):
+            return
+
+        if base_f is not None:
+            base_f = float(base_f)
+        else:
+            if self.factor is None:
+                _logger.error('Cannot flip_at_bound without factor')
+                return
+            base_f = self.factor if self.factor >= 1.0 else 1 / self.factor
+
+        if num_clients >= self.max_num_clients:
+            self.factor = 1 / base_f
+        else:
+            self.factor = base_f
+
+        _logger.debug('Factor set to %g (num_clients: %d)', self.factor,
+                      num_clients)
+
+    def _parse(self, program_text):
+        program = []
+        for line in re.split(r'\s*\n\s*', program_text):
+            if line == '' or line.startswith('#'):
+                continue
+            program.append(re.split(r'\s+', line))
+        _logger.debug("Parsed program %s" % program)
+        return program
+
+
 def _startup(runner, fn):
     controller = Controller(runner)
     controller_available.fire(controller)
     if fn:
         fn(controller)
+    elif _config_program:
+        handler = ProgrammedHandler(program=_config_program)
+        handler.run(controller)
+    _logger.debug('Abandoning control greenlet')
 
 
 def _controller_poll_runner(fn):
